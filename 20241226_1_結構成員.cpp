11bf06a6d685f6ca2e1ca4c863fdaf75935934a3
x13x123 <313013@s.lhvs.tyc.edu.tw>
2024-12-26 14:23:34 +0800

    Create 20241226_1_結構成員.cpp

---
+++ 20241226_1_結構成員.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+class myTime{
+private:
+	int hour;
+	int minute;
+	bool validTime(int h,int m);
+public:
+	bool setTime(int h,int m);
+	int getHour(){return hour;}
+	int getMinute(){return minute;}
+	void printTime();
+};
+
+bool myTime::validTime(int h, int m){
+	if(h < 0 || h > 23)return false;
+	if(m < 0 || m > 59)return false;
+	return true;	
+}
+bool myTime::setTime(int h, int m){
+	if(validTime(h,m)){
+	hour = h;
+	minute = m;
+	return true;
+	}
+	else return false; 
+}
+void myTime::printTime(){
+	cout<<hour<<":"<<minute<<"\n";
+}
+
+main(){
+	myTime open, close, now;
+	int h, m;
+	open.setTime(10,30);
+	close.setTime(17,30);
+	now.setTime(13,28);
+	cout<<"開張時間:";
+	open.printTime();
+	cout<<"結束時間:";
+	close.printTime();
+	cout<<"現在時間:";
+	h = now.getHour();
+	m = now.getMinute();
+	cout<<h<<":"<<m<<"\n";
+}
